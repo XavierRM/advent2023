ef6ef2883bdb2d18fadbbe107b84bf749c33dd4d
XavierRM <xavier.rodriguez.munoz@gmail.com>
2023-12-21 21:46:02 +0100

    Added solution for the first challenge of Day One - Added description on how to build and run a subproject - Day one - 1 Solution

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ Each file will contain the resolution of one challenge, corresponding each, to t
 The template directory contains, as it's name states, a template OCaml subproject used to start every challenge.
 
 The tag ```startingpoint``` has a version of the project that is ready to use resolving challenges.
+
+# Build and run
+
+To build one of the subprojects just run something like:
+
+```
+dune build template
+```
+
+To run the executable, execute the following command:
+
+```
+dune exec template
+```
--- day-one/dayone.ml
+++ day-one/dayone.ml
@@ -1,1 +1,30 @@
-let () = print_endline "This is a template"
+open List
+open Printf
+
+let file = "input.txt"
+
+let int_value c = int_of_char c - int_of_char '0'
+let lineNum digitList =
+  if (length digitList) == 1 then
+    (nth digitList 0)*10 + (nth digitList 0) 
+  else 
+    (nth digitList 0) + 10*(nth digitList ((length digitList)-1))
+
+let parseLine line = 
+  let rec auxParseLine line nnumbers acum =
+    let currentChar = line.[0] in
+      match currentChar, String.length line with
+      | '0'..'9', 1 -> (int_value currentChar)::acum
+      | _, 1-> acum
+      | '0'..'9', _ -> auxParseLine (String.sub line 1 ((String.length line)-1)) (nnumbers/10) ((int_value currentChar)::acum) 
+      | _, _ -> auxParseLine (String.sub line 1 ((String.length line)-1)) nnumbers acum
+  in auxParseLine line 10 [];;
+
+let rec auxParse sum file =
+  match input_line file with
+    | line -> auxParse (sum + (lineNum (parseLine line))) file
+    | exception End_of_file -> sum
+
+let parseFile file = auxParse 0 file;;
+
+let () = printf "%i\n" (parseFile (open_in file))
